2f5f09ec870e484e1a812f5b49b3d8ed65a435fb
Shane Daniel <shanedaniel@Shanes-MacBook-Pro.local>
2021-12-26 03:48:04 -0500

    First Commit

---
+++ src/main/java/com/big/imagination/small/stories/big_imagination_small_stories/BigImaginationSmallStoriesApplication.java
@@ -0,0 +1,13 @@
+package com.big.imagination.small.stories.big_imagination_small_stories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigImaginationSmallStoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigImaginationSmallStoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/big/imagination/small/stories/big_imagination_small_stories/BigImaginationSmallStoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.big.imagination.small.stories.big_imagination_small_stories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigImaginationSmallStoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
